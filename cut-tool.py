16a28294a29c9d112bc4b1e32bd192941d33251e
acleitner1 <78944987+acleitner1@users.noreply.github.com>
2024-06-03 08:32:27 -0400

    d supported: Step 2 complete

--- cut-tool.py
+++ cut-tool.py
@@ -6,12 +6,20 @@ import sys
 def main(input): 
    if (len(input.argv) < 3): 
       print("Must specify an option and a filename") 
-   option = input.argv[1]    
-   file = input.argv[2]
+   options = "" 
+   delimiter = "\t"
+   for i in range(len(input.argv)): 
+      if input.argv[i][0:2] == "-f": 
+         option = input.argv[i]
+      elif input.argv[i][0:2] == "-d": 
+         delimiter = input.argv[i][2:]
+      else: 
+         file = input.argv[i]
+
    if (option[0:2] == "-f"): 
       f = open(file, "r")
       for line in f: 
-         line = line.split("\t")
+         line = line.split(delimiter)
          print(line[int(option[2])])
 
 main(sys)   
